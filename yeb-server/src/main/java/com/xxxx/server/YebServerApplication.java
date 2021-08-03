a83c88ceb8dc9ad0cbfd93140f22203216d3cc93
李毅恒 <834805011@qq.com>
2021-08-03 09:38:15 +0800

    环境搭建

--- yeb-server/src/main/java/com/xxxx/server/YebServerApplication.java
+++ yeb-server/src/main/java/com/xxxx/server/YebServerApplication.java
@@ -1,4 +1,4 @@
-package com.xxxx.yebserver;
+package com.xxxx.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- yeb-server/src/test/java/com/xxxx/server/YebServerApplicationTests.java
+++ yeb-server/src/test/java/com/xxxx/server/YebServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.xxxx.yebserver;
+package com.xxxx.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
