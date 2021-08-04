a37342c9ef504e0ea9ff71f6d5e918c4a8cbe51f
李毅恒 <834805011@qq.com>
2021-08-04 09:59:22 +0800

    登陆功能

---
+++ yeb-server/src/main/java/com/xxxx/server/controller/LoginController.java
@@ -0,0 +1,10 @@
+package com.xxxx.server.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "LoginController")
+@RestController
+public class LoginController {
+
+}
