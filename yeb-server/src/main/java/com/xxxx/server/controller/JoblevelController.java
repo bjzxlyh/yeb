6be41986f23d70d2991e9cecc145b873694da398
李毅恒 <834805011@qq.com>
2021-08-06 15:53:48 +0800

    基础信息设置模块

--- yeb-server/src/main/java/com/xxxx/server/controller/JoblevelController.java
+++ yeb-server/src/main/java/com/xxxx/server/controller/JoblevelController.java
@@ -1,9 +1,9 @@
 package com.xxxx.server.controller;
 
 
-import com.xxxx.server.pojo.Position;
+import com.xxxx.server.pojo.Joblevel;
 import com.xxxx.server.pojo.RespBean;
-import com.xxxx.server.service.IPositionService;
+import com.xxxx.server.service.IJoblevelService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,53 +21,53 @@ import java.util.List;
  * @since 2021-08-03
  */
 @RestController
-@RequestMapping("/system/basic/pos")
-public class PositionController {
-
+@RequestMapping("/system/basic/joblevel")
+public class JoblevelController {
     @Autowired
-    private IPositionService positionService;
+    private IJoblevelService joblevelService;
 
-
-    @ApiOperation(value = "获取所有职位信息")
+    @ApiOperation(value = "获取所有职称")
     @GetMapping("/")
-    public List<Position> getAllPositions(){
-        return positionService.list();
+    public List<Joblevel> getAllJobLevels(){
+        return joblevelService.list();
     }
-    @ApiOperation(value = "添加职位信息")
+
+    @ApiOperation(value = "添加职称")
     @PostMapping("/")
-    public RespBean addPosition(@RequestBody Position position){
-        position.setCreateDate(LocalDateTime.now());
-        if (positionService.save(position)){
+    public RespBean addJobLevel(@RequestBody Joblevel joblevel){
+        joblevel.setCreateDate(LocalDateTime.now());
+        if (joblevelService.save(joblevel)){
             return RespBean.success("添加成功");
         }
         return RespBean.error("添加失败");
     }
 
-    @ApiOperation(value = "更新职位信息")
+    @ApiOperation(value = "更新职称")
     @PutMapping("/")
-    public RespBean updatePosition(@RequestBody Position position){
-        if (positionService.updateById(position)){
+    public RespBean updateJobLevel(@RequestBody Joblevel joblevel){
+        if (joblevelService.updateById(joblevel)){
             return RespBean.success("更新成功");
         }
         return RespBean.error("更新失败");
     }
 
-    @ApiOperation(value = "删除职位信息")
+    @ApiOperation(value = "删除职称")
     @DeleteMapping("/{id}")
-    public RespBean deletePosition(@PathVariable Integer id){
-        if (positionService.removeById(id)){
+    public RespBean deleteJobLevel(@PathVariable Integer id){
+        if (joblevelService.removeById(id)){
             return RespBean.success("删除成功");
         }
         return RespBean.error("删除失败");
     }
 
-    @ApiOperation(value = "批量删除职位信息")
+    @ApiOperation(value = "批量删除职称")
     @DeleteMapping("/")
-    public RespBean deletePositionsByIds(Integer[] ids){
-        if (positionService.removeByIds(Arrays.asList(ids))){
+    public RespBean deleteJobLevelsByIds(Integer[] ids){
+        if (joblevelService.removeByIds(Arrays.asList(ids))){
             return RespBean.success("删除成功");
         }
         return RespBean.error("删除失败");
+
     }
 
 }
---
+++ yeb-server/src/main/java/com/xxxx/server/pojo/Joblevel.java
@@ -0,0 +1,53 @@
+package com.xxxx.server.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
+import com.baomidou.mybatisplus.annotation.TableField;
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author 李毅恒
+ * @since 2021-08-03
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("t_joblevel")
+@Accessors(chain = true)
+@ApiModel(value="Joblevel对象", description="")
+public class Joblevel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "id")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty(value = "职称名称")
+    private String name;
+
+    @ApiModelProperty(value = "职称等级")
+    @TableField("titleLevel")
+    private String titleLevel;
+
+    @ApiModelProperty(value = "创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "Asia/Shanghai")
+    private LocalDateTime createDate;
+
+    @ApiModelProperty(value = "是否启用")
+    private Boolean enabled;
+
+
+}
